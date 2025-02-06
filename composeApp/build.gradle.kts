55f298db89cc808c207fc7167d1e8a1bfbf7f5d5
Antonio Corrales <desarrollodroide@gmail.com>
2025-02-06 08:50:17 +0100

    Refactor: Remove unused code and update dependencies
    This commit removes unused code related to desktop and wasm targets, and updates dependencies in `gradle/libs.versions.toml`.
    
    -   Removes `index.html` and `styles.css` for wasm target.
    -   Removes `main.kt` and `Platform.jvm.kt` for desktop target.
    -   Removes `main.kt` and `Platform.wasmJs.kt` for wasm target.
    -   Updates dependencies versions in `gradle/libs.versions.toml`.
    -   Updates gradle plugins in `build.gradle.kts` and `composeApp/build.gradle.kts`.
    - Removes wasm and jvm target
    - Update dependencies version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,19 +1,16 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
@@ -30,31 +27,8 @@ kotlin {
         }
     }
     
-    jvm("desktop")
-    
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        moduleName = "composeApp"
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                outputFileName = "composeApp.js"
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-        binaries.executable()
-    }
-    
     sourceSets {
-        val desktopMain by getting
-        
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
@@ -67,12 +41,8 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation("org.jetbrains.compose.material3:material3:1.7.3")
-        }
-        desktopMain.dependencies {
-            implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutines.swing)
         }
     }
 }
@@ -108,14 +78,3 @@ dependencies {
     debugImplementation(compose.uiTooling)
 }
 
-compose.desktop {
-    application {
-        mainClass = "com.desarrollodroide.adventurelog.MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.desarrollodroide.adventurelog"
-            packageVersion = "1.0.0"
-        }
-    }
-}
